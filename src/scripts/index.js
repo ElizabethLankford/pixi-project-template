e50eaa667c3690009458447043148f46ca38a462
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-12 18:49:23 -0600

    added all files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import { App } from "./system/App";
 import { Config } from "./game/Config";
 
-App.run(Config);
+//App.run(Config);
--- src/scripts/system/ScenesManager.js
+++ src/scripts/system/ScenesManager.js
@@ -2,18 +2,24 @@ import * as PIXI from "pixi.js";
 import { App } from "./App";
 
 export class ScenesManager {
-    constructor() {
-        this.container = new PIXI.Container();
-        this.container.interactive = true;
-        this.scene = null;
+  constructor() {
+    this.container = new PIXI.Container();
+    this.container.interactive = true;
+    this.scene = null;
+  }
+
+  start(scene) {
+    if (this.scene) {
+      this.scene.destroy();
     }
 
-    start(scene) {
-        if (this.scene) {
-            this.scene.remove();
-        }
+    this.scene = new App.config.scenes[scene]();
+    this.container.addChild(this.scene.container);
+  }
 
-        this.scene = new App.config.scenes[scene]();
-        this.container.addChild(this.scene.container);
+  update(dt) {
+    if (this.scene && this.scene.update) {
+      this.scene.update(dt);
     }
+  }
 }
